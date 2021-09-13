7ddc9f582a935e047a0831be118395acd3241bdc
Muhammad Talha Abbas <70336405+mtalhaabbas@users.noreply.github.com>
2021-09-13 13:53:54 +0500

    update commit

--- QRCode/settings.py
+++ QRCode/settings.py
@@ -113,13 +113,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-SITE_URL = 'http://127.0.0.1:8000/'
+SITE_URL = 'https://get-qr-code.herokuapp.com/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
